f9b7890e949d56bcd1b1214e3f66d603dd277ea1
Daniel Arenas <daniel1144151@gmail.com>
2025-11-02 15:56:59 +0000

    validate sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, unless: :devise_controller?
+  before_action :authenticate_user!, if: :validate_user?
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   # before_action -> { sleep 1 }
 
   private
+
+  def validate_user?
+    current_user.nil? & current_admin_user.nil?
+  end
 
   def current_company
     @current_company ||= current_user.company if user_signed_in?

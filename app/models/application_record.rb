2fb5306e2031b20da46b95329a925970aea64e06
Daniel Arenas <daniel1144151@gmail.com>
2025-11-02 15:57:34 +0000

    add active admisn configuration

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def self.ransackable_attributes(auth_object = nil)
+    self.column_names
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    self.reflect_on_all_associations.collect{|r| r.name.to_s}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   devise_for :users
   get "up" => "rails/health#show", as: :rails_health_check
 

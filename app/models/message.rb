ec1b97e5f1a273aafd39e883e77b941e07b8d00b
Daniel Arenas <daniel1144151@gmail.com>
2025-11-06 23:09:27 +0000

    add mockup

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,15 @@ class Message < ApplicationRecord
     order(created_at: :desc)
   }
 
+  def assistant?
+    role == "assistant"
+  end
+
+  def user?
+    role == "user"
+  end
+
+  def user_name
+    user? ? conversation.lead.name : conversation.assistant.name
+  end
 end

dd66612f7782a3faf23324bf2c5c7d58f0bbad9f
Daniel Arenas <daniel1144151@gmail.com>
2025-11-17 20:49:26 +0000

    fix index table real estate

--- app/admin/company/real_estate_admin.rb
+++ app/admin/company/real_estate_admin.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register RealEstate do
 
+  index do
+    selectable_column
+    id_column
+    column :company
+    column :name
+    column :code
+    column :url
+    column :price
+
+    actions
+  end
 end

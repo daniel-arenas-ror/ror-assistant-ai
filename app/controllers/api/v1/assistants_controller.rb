85ed8ba61f40e5b441df73374ecd208c0be5cc1b
Daniel Arenas <daniel1144151@gmail.com>
2025-11-21 13:35:16 +0000

    fix assistant controller

--- app/controllers/api/v1/assistants_controller.rb
+++ app/controllers/api/v1/assistants_controller.rb
@@ -2,9 +2,7 @@ module Api
   module V1
     class AssistantsController < BaseController
       def show
-        p " params #{params}"
-
-        render json: { message: "Assistant details" }
+        render json:  Assistant.find_by_slug(params[:id])
       end
     end
   end

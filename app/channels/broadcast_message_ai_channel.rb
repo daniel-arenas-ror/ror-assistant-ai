515a88a86df15dd984fc6743c45347143ee4c007
Daniel Arenas <daniel1144151@gmail.com>
2025-11-21 23:42:08 +0000

    wip implement chat with AI

--- app/channels/broadcast_message_ai_channel.rb
+++ app/channels/broadcast_message_ai_channel.rb
@@ -1,32 +1,20 @@
 class BroadcastMessageAiChannel < ApplicationCable::Channel
   def subscribed
-    p " ** subscribed ** "
-    # stream_from "some_channel"
-    p "parameters: #{params}"
-    stream_for "broadcast_message_ai_channel_#{params['conversation_id']}"
-  end
+    assistant = Assistant.find_by_slug(params[:assistant_id])
+    conversation = AIService::OpenaiService::Conversations.new(
+        assistant: assistant
+      ).add_message(params[:message])
 
-  def receive(data)
-    p " ** receive ** "
-    p "data received in receive: #{data}"
-
-    ActionCable.server.broadcast("broadcast_message_ai_channel", data)
+    stream_for "broadcast_message_ai_channel_#{assistant.slug}_#{conversation.id}"
   end
 
   def speak(data)
-    p " ** speak ** "
-    p params
 
-    p " data in speak: #{data["message"]} "
+    conversation = AIService::OpenaiService::Conversations.new(
+      conversation: conversation
+    ).add_message(params[:message])
 
-    broadcast_to "broadcast_message_ai_channel_#{params['conversation_id']}", { type: 'typing_start', content: data["message"] }
-
-    sleep 5
-
-    broadcast_to "broadcast_message_ai_channel_#{params['conversation_id']}", { type: 'message_chunk', content: data["message"] }
-
-    sleep 2
-    broadcast_to "broadcast_message_ai_channel_#{params['conversation_id']}", { type: 'typing_end', content: data["message"] }
+    broadcast_to "broadcast_message_ai_channel_#{assistant.slug}_#{conversation.id}", { type: 'typing_end', content: data["message"] }
   end
 
   def unsubscribed

352c4e216982c9cc7073fec60a1211e22458619f
Daniel Arenas <daniel1144151@gmail.com>
2025-11-24 18:37:24 +0000

    add details of lead

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -5,6 +5,7 @@ class ConversationsController < ApplicationController
 
   def show
     @conversation = current_company.conversations.find(params[:id])
+    @lead = @conversation.lead
     @messages = @conversation.messages.order(created_at: :asc)
   end
 end

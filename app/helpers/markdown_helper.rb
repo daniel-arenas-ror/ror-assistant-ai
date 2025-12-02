0f4e21e90bd94edc3146984591eba1ccd325cf8f
Daniel Arenas <daniel1144151@gmail.com>
2025-12-02 12:08:01 +0000

    setup render

---
+++ app/helpers/markdown_helper.rb
@@ -0,0 +1,23 @@
+module MarkdownHelper
+
+  RENDER_OPTIONS = {
+    filter_html: true,
+    hard_wrap: true,
+    link_attributes: { rel: "nofollow" }
+  }
+
+  EXTENSIONS = {
+    autolink: true,
+    tables: true,
+    fenced_code_blocks: true,
+    strikethrough: true,
+    space_after_headers: true
+  }
+
+  def render_markdown(text)
+    renderer = CustomMarkdownRenderer.new(RENDER_OPTIONS)
+    markdown = Redcarpet::Markdown.new(renderer, EXTENSIONS)
+
+    markdown.render(text).html_safe
+  end
+end

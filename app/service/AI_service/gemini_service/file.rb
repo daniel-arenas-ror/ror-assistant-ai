c9c895449db84d0798738b6a552e7d11a01a2787
Daniel Arenas <daniel1144151@gmail.com>
2025-12-06 23:55:36 +0000

    add file service

---
+++ app/service/AI_service/gemini_service/file.rb
@@ -0,0 +1,39 @@
+module AIService
+  module GeminiService
+    class File < Base
+      FILE_API_BASE_URL = "https://generativelanguage.googleapis.com/v1beta/files"
+
+      def initialize(api_key)
+        super(ENV.fetch('GEMINI_API_KEY', ''))
+
+      end
+
+      def upload_file(file_path:)
+        payload = {
+          file: {
+            content: Base64.strict_encode64(File.read(file_path)),
+            filename: File.basename(file_path),
+            mime_type: "application/octet-stream"
+          }
+        }
+
+        response_data = make_api_call(url: FILE_API_BASE_URL, payload: payload)
+
+        response_data
+      end
+
+      def delete_file(file_id:)
+        url = "#{FILE_API_BASE_URL}/#{file_id}"
+        response = make_api_call(url, method: :delete)
+
+        if response.code == 200
+          { message: "File deleted successfully." }
+        else
+          raise "API Error: #{response.code} - #{response.body}"
+        end
+      end
+
+      
+    end
+  end
+end

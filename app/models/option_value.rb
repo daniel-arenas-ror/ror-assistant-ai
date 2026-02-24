af56a9a51fa0f92523ec48eabf554c2c58d9b2ea
Daniel Arenas <daniel1144151@gmail.com>
2026-02-23 20:30:39 -0500

    update variant relation

--- app/models/option_value.rb
+++ app/models/option_value.rb
@@ -2,5 +2,8 @@ class OptionValue < ApplicationRecord
   belongs_to :company
   belongs_to :option_type
 
+  has_many :option_value_variants, dependent: :destroy
+  has_many :variants, through: :option_value_variants
+
   validates :name, presence: true
 end

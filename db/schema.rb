a38c4d4c481c9298083f47be723edad4b6ad6019
Daniel Arenas <daniel1144151@gmail.com>
2026-02-24 22:23:51 -0500

    add image migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_02_19_015725) do
+ActiveRecord::Schema[8.1].define(version: 2026_02_25_032314) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "vector"
@@ -27,6 +27,34 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_19_015725) do
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource"
+  end
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.string "name", null: false
+    t.bigint "record_id", null: false
+    t.string "record_type", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.string "content_type"
+    t.datetime "created_at", null: false
+    t.string "filename", null: false
+    t.string "key", null: false
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "admin_users", force: :cascade do |t|
@@ -162,19 +190,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_19_015725) do
     t.index ["option_type_id"], name: "index_option_values_on_option_type_id"
   end
 
-  create_table "products", force: :cascade do |t|
-    t.text "amenities"
-    t.string "code"
-    t.bigint "company_id", null: false
-    t.datetime "created_at", null: false
-    t.text "description"
-    t.string "location"
-    t.string "name"
-    t.string "price"
-    t.datetime "updated_at", null: false
-    t.string "url"
-    t.jsonb "url_images"
-  end
+# Could not dump table "products" because of following StandardError
+#   Unknown type 'vector' for column 'embedding'
+
 
   create_table "quotes", force: :cascade do |t|
     t.integer "company_id"
@@ -244,6 +262,8 @@ ActiveRecord::Schema[8.1].define(version: 2026_02_19_015725) do
     t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "assistant_files", "assistants"
   add_foreign_key "assistant_tools", "assistants"
   add_foreign_key "assistant_tools", "tools"

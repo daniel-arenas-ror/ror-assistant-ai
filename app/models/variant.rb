3f0759cef58d8a516ad028186a48737165cf5b4d
Daniel Arenas <daniel1144151@gmail.com>
2026-02-24 22:24:11 -0500

    add variant image configuration

--- app/models/variant.rb
+++ app/models/variant.rb
@@ -7,4 +7,10 @@ class Variant < ApplicationRecord
   has_many :option_types, -> { distinct }, through: :option_values
 
   accepts_nested_attributes_for :variant_option_values, allow_destroy: true
+
+  has_many_attached :images do |attachable|
+    attachable.variant :thumb, resize_to_limit: [100, 100]
+    attachable.variant :medium, resize_to_limit: [500, 500]
+    attachable.variant :large, resize_to_limit: [1000, 1000]
+  end
 end

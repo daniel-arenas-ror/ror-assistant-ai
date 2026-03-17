73c38b09278e065105b911ca0ee360bce75b9af0
Daniel Arenas <daniel1144151@gmail.com>
2026-03-16 22:06:44 -0500

    add product detail configuration

--- app/graphql/types/company_type.rb
+++ app/graphql/types/company_type.rb
@@ -8,15 +8,13 @@ module Types
     field :name, String, null: false
     field :icon_url, String, null: true, description: "URL of the company's icon image."
     field :company_item_configurations, [CompanyItemConfigurationsType], null: true, description: "Item configurations for the company."
+    field :categories, CategoryType.connection_type, null: true, description: "Root categories belonging to the company."
     field :product_card_configuration, String, null: true, description: "Product card configuration for the company."
+    field :product_detail_configuration, String, null: true, description: "Product detail configuration for the company."
 
     def product_card_configuration
       object.company_item_configurations.find_by(name: "ProductCardStyle")&.value
     end
-
-    # associations we might need
-    field :categories, CategoryType.connection_type, null: true,
-          description: "Root categories belonging to the company."
 
     def categories
       object.categories.where(parent_id: nil).order(:name)
@@ -24,6 +22,10 @@ module Types
 
     def icon_url
       Rails.application.routes.url_helpers.rails_blob_url(object.icon) if object.icon.attached?
+    end
+
+    def product_detail_configuration
+      object.company_item_configurations.find_by(name: "ProductDetailStyle")&.value
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_12_000535) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_15_005001) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "vector"
@@ -223,6 +223,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_12_000535) do
   create_table "option_values", force: :cascade do |t|
     t.bigint "company_id", null: false
     t.datetime "created_at", null: false
+    t.string "label"
     t.string "name"
     t.bigint "option_type_id", null: false
     t.datetime "updated_at", null: false

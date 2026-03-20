7e770ccabfea74772f25121d16fa378298dda2fc
Daniel Arenas <daniel1144151@gmail.com>
2026-03-19 21:03:10 -0500

    group option values

--- app/graphql/types/product_type.rb
+++ app/graphql/types/product_type.rb
@@ -13,10 +13,27 @@ module Types
     field :active, Boolean, null: false
     field :all_images, [Types::ImageType], null: true
     field :option_values, [Types::OptionValueType], null: true
+    field :grouped_option_values, [Types::OptionType], null: false
     field :variants, [Types::VariantType], null: true
 
     def all_images
       object.images + object.variants.flat_map(&:images) + object.product_option_values.flat_map(&:images)
     end
+
+    def option_values
+      object.option_values
+    end
+
+  def grouped_option_values
+    values = option_values.includes(:option_type)
+
+    values.group_by(&:option_type).map do |type, values_list|
+      {
+        id: type.id,
+        name: type.name,
+        option_values: values_list.map { |v| { id: v.id, name: v.name, label: v.label } }
+      }
+    end
+  end
   end
 end

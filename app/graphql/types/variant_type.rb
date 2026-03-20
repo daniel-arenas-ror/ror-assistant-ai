2259a1af1e735234d210e82e2c54316eed29f9d4
Daniel Arenas <daniel1144151@gmail.com>
2026-03-19 21:18:51 -0500

    add relation images to variants

--- app/graphql/types/variant_type.rb
+++ app/graphql/types/variant_type.rb
@@ -8,5 +8,8 @@ module Types
     field :option_values, [Types::OptionValueType], null: true
     field :images, [Types::ImageType], null: true
 
+    def images
+      object.images + object.product.product_option_values.select{|opv| object.option_value_ids.include?(opv.option_value_id)}.flat_map(&:images)
+    end
   end
 end

53602f9b416d77260065967f5a921801add9e1ef
Daniel Arenas <daniel1144151@gmail.com>
2026-03-20 21:11:13 -0500

    update migration to add default price

--- db/migrate/20260219013232_create_variants.rb
+++ db/migrate/20260219013232_create_variants.rb
@@ -3,7 +3,7 @@ class CreateVariants < ActiveRecord::Migration[8.1]
     create_table :variants do |t|
       t.references :product, null: false, foreign_key: true
       t.string :sku
-      t.float :price
+      t.float :price, null: false, default: 0
 
       t.timestamps
     end

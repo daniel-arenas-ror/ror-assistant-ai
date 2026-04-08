16b3e4b5bd3a30184be4b0176b5725d330693204
Daniel Arenas <daniel1144151@gmail.com>
2026-04-07 21:35:46 -0500

    add cart resolver

---
+++ app/graphql/mutations/add_to_cart.rb
@@ -0,0 +1,33 @@
+module Mutations
+  class AddToCart < BaseMutation
+    argument :variant_id, ID, required: true
+    argument :quantity, Integer, required: true
+
+    type Types::CartType
+
+    def authenticate_user!
+      context[:current_user] || raise(GraphQL::ExecutionError, "Authentication required")
+    end
+
+    def resolve(variant_id:, quantity:)
+      user = context[:current_user]
+
+      cart = user.cart || user.create_cart
+      cart_item = cart.cart_items.find_or_initialize_by(variant_id: variant_id)
+
+      if cart_item.new_record?
+        cart_item.quantity = quantity
+      else
+        cart_item.quantity += quantity
+      end
+
+      if cart_item.save
+        # Recalculate and persist the grand total
+        cart.update_total!
+        cart
+      else
+        raise GraphQL::ExecutionError, cart_item.errors.full_messages.join(", ")
+      end
+    end
+  end
+end

522244aa34f23881bb77a2a7d356f8a93979b599
Daniel Arenas <daniel1144151@gmail.com>
2026-04-09 21:56:02 -0500

    update default value

--- app/graphql/queries/cart_query.rb
+++ app/graphql/queries/cart_query.rb
@@ -3,11 +3,11 @@ module Queries
     type Types::CartType, null: true
     argument :company_id, ID, required: true
 
-    def resolve(company_id: )
+    def resolve(company_id: , **)
       user = context[:current_user]
       return nil unless user
 
-      user.cart || user.create_cart
+      user.carts.find_by(company_id: company_id) || user.carts.create!(company_id: company_id)
     end
   end
 end
--- db/migrate/20260408012500_create_carts.rb
+++ db/migrate/20260408012500_create_carts.rb
@@ -3,8 +3,8 @@ class CreateCarts < ActiveRecord::Migration[8.1]
     create_table :carts do |t|
       t.references :lead, null: false, foreign_key: true
       t.references :company, null: false, foreign_key: true
-      t.monetize :total
-      t.monetize :sub_total
+      t.monetize :total, default: 0, null: false
+      t.monetize :sub_total, default: 0, null: false
 
       t.timestamps
     end

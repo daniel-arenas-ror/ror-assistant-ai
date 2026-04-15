0a010480bb50bd2fd066b231dfb7a791bc465471
Daniel Arenas <daniel1144151@gmail.com>
2026-04-14 20:26:24 -0500

    add states machine to orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,25 @@ class Order < ApplicationRecord
   belongs_to :company
   belongs_to :lead
 
+  has_many :payment_transitions, class_name: "OrderPaymentTransition", autosave: false
+  has_many :delivery_transitions, class_name: "OrderDeliveryTransition", autosave: false
+
   monetize :total_cents, as: :total
   monetize :sub_total_cents, as: :sub_total
+
+  def payment_machine
+    @payment_machine ||= PaymentStateMachine.new(self, transition_class: OrderPaymentTransition)
+  end
+
+  def delivery_machine
+    @delivery_machine ||= DeliveryStateMachine.new(self, transition_class: OrderDeliveryTransition)
+  end
+
+  def payment_status
+    payment_machine.current_state
+  end
+
+  def delivery_status
+    delivery_machine.current_state
+  end
 end

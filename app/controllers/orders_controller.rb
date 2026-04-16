37d8836526318317dec78b46294575e18e45ae2d
Daniel Arenas <daniel1144151@gmail.com>
2026-04-15 21:05:17 -0500

    add order index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
   def index
-    @orders = current_company.orders.order(created_at: :desc)
+    @orders = current_company.orders.includes(:lead).order(created_at: :desc)
   end
 end

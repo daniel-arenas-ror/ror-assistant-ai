870b9b0ecd1f6ee5d2af34a451f6ad88c6c430e5
Daniel Arenas <daniel1144151@gmail.com>
2026-04-16 22:25:17 -0500

    update transitions models

--- app/models/order_delivery_transition.rb
+++ app/models/order_delivery_transition.rb
@@ -1,2 +1,5 @@
 class OrderDeliveryTransition < ApplicationRecord
+  validates :to_state, inclusion: { in: DeliveryStateMachine.states }
+
+  belongs_to :order, inverse_of: :delivery_transitions
 end
--- app/models/order_payment_transition.rb
+++ app/models/order_payment_transition.rb
@@ -1,2 +1,5 @@
 class OrderPaymentTransition < ApplicationRecord
+  validates :to_state, inclusion: { in: PaymentStateMachine.states }
+
+  belongs_to :order, inverse_of: :payment_transitions
 end

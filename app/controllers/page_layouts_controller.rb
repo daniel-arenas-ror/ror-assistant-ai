e560250817595ece8df4bfc0bb0abe4df0667ebe
Daniel Arenas <daniel1144151@gmail.com>
2026-04-27 21:12:14 -0500

    add skeleton page layouts

--- app/controllers/page_layouts_controller.rb
+++ app/controllers/page_layouts_controller.rb
@@ -2,4 +2,35 @@ class PageLayoutsController < ApplicationController
   def index
     @page_layouts = current_company.page_layouts.draft.order(created_at: :desc)
   end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def send_to_production
+
+  end
+
+  private
+
+  def page_layout_params
+    params.require(:page_layout).permit(
+      :page_type,
+      :meta_title,
+      :path,
+      :version
+    )
+  end
 end

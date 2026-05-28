f7fabb6023be236b874e7082dfe36fb4b46ebd2e
Daniel Arenas <daniel1144151@gmail.com>
2026-05-27 19:31:58 -0500

    update form

--- app/javascript/controllers/tabs_controller.js
+++ app/javascript/controllers/tabs_controller.js
@@ -1,51 +1,69 @@
-import { Controller } from "@hotwired/stimulus";
+import { Controller } from "@hotwired/stimulus"
+
+const ACTIVE_HEADER_CLASSES = [
+  "border-blue-600",
+  "text-blue-600",
+  "font-semibold"
+]
+
+const INACTIVE_HEADER_CLASSES = [
+  "border-transparent",
+  "text-gray-500"
+]
 
 export default class extends Controller {
-  static targets = [ "header", "body" ]
+  static targets = ["header", "body"]
 
   initialize() {
+    console.log("initialize")
     this.index = 1
     this.showCurrentSlide()
   }
 
   next(event) {
-    this.index++
     event.preventDefault()
-    this.showCurrentSlide()
+    if (this.index < this.headerTargets.length) {
+      this.index++
+      this.showCurrentSlide()
+    }
   }
 
   previous(event) {
-    this.index--
     event.preventDefault()
+    if (this.index > 1) {
+      this.index--
+      this.showCurrentSlide()
+    }
+  }
+
+  switch(event) {
+    event.preventDefault()
+    this.index = Number(event.currentTarget.dataset.step)
     this.showCurrentSlide()
   }
 
   showCurrentSlide() {
-    this.cleanActiveClasses();
+    const index = Number(this.index)
 
     this.headerTargets.forEach((element) => {
-      if (element.dataset.step == this.index){
-        element.classList.add("form-stepper__step--active")
+      element.classList.remove(...ACTIVE_HEADER_CLASSES)
+      element.classList.add(...INACTIVE_HEADER_CLASSES)
+    })
+
+    this.bodyTargets.forEach((element) => {
+      element.classList.add("hidden")
+    })
+
+    this.headerTargets.forEach((element) => {
+      if (Number(element.dataset.step) === index) {
+        element.classList.remove(...INACTIVE_HEADER_CLASSES)
+        element.classList.add(...ACTIVE_HEADER_CLASSES)
       }
     })
 
-    document
-        .querySelector(`[data-step-content="${this.index}"]`)
-        .classList.add("form-stepper__content-pane--active");
-  }
-
-  switch(event) {
-    this.index = event.currentTarget.dataset.step;
-    this.showCurrentSlide()
-  }
-
-  cleanActiveClasses() {
-    this.headerTargets.forEach((element) => {
-      element.classList.remove("form-stepper__step--active")
-    })
-
-    this.bodyTargets.forEach(el =>
-      el.classList.remove("form-stepper__content-pane--active")
-    );
+    const activePane = this.bodyTargets.find(
+      (element) => Number(element.dataset.stepContent) === index
+    )
+    activePane?.classList.remove("hidden")
   }
 }
